4d93593c1cfb6073c75e0fd0e6a82ed48623e7aa
nashcft <ltz9.71logmc.d5s@gmail.com>
2016-05-03 18:04:39 +0900

    change left Quote to SYMB.

--- keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   [    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  '"  | LGui |AltShf| Alt  |  =+  |                                       | Left | Down |  Up  | Right|   ]  |
+ *   | ~L1  | LGui |AltShf| Alt  |  =+  |                                       | Left | Down |  Up  | Right|   ]  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
         KC_LCTL,  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,  KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_QUOT,  KC_LGUI,      LALT(KC_LSFT),  KC_LALT,KC_EQL,
+        KC_FN1,   KC_LGUI,      LALT(KC_LSFT),  KC_LALT,KC_EQL,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                               KC_SPC, KC_BSPC,KC_DELT,
