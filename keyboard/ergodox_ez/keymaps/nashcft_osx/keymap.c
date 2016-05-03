47af12fec7aeb259c3615739704dc08587146df6
nashcft <ltz9.71logmc.d5s@gmail.com>
2016-05-03 17:03:25 +0900

    swap up-key with down-key.

--- keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | = / +  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   [    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Grv  |  '"  |AltShf| LGui |BkSpc |                                       | Left |  Up  | Down | Right|   ]  |
+ *   | Grv  |  '"  |AltShf| LGui |BkSpc |                                       | Left | Down |  Up  | Right|   ]  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(1),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_LBRC,
-                                  KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,          KC_RBRC,
+                                  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_RBRC,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_RSFT,KC_ENT
