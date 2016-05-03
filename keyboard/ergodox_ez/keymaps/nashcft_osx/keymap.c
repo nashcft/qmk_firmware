61edca618f7dda8c09b6f555a9b0d0a48c446ad6
nashcft <ltz9.71logmc.d5s@gmail.com>
2016-05-03 18:05:45 +0900

    change right-LALT to RALT.

--- keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_LBRC,
                                   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_RBRC,
-             KC_LALT,        CTL_T(KC_ESC),
+             KC_RALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_PGDN,KC_RSFT,KC_ENT
     ),
