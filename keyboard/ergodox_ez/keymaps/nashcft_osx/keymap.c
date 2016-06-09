21183e1c12d9811da0cf693488563dd173cd7e84
nashcft <ltz9.71logmc.d5s@gmail.com>
2016-06-09 10:22:43 +0900

    Remove Del and BS keys on symbol layer

--- keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/nashcft_osx/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   %  | Del  |   [  |   ]  |  BS  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |  Del |BkSpc |                                       |      |    . |   0  |   =  |      |
+ *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_HASH,KC_DLR, KC_PGDN,KC_RPRN,KC_HOME,
        KC_TRNS,KC_PERC,KC_DELT,KC_LBRC,KC_RBRC,KC_BSPC,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_DELT,KC_BSPC,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
